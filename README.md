c954333d3b66cb0da10d8e2a778edb24e0eb843d
Agersonlopezb <agersonlopez@outlook.com>
2025-05-04 06:38:23 -0600

    data changes

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ttAaronsproject
 Repository for Aaron's project regarding Data analyst Tripleten Sprint7
+Este proyecto corresponde al Sprint 7 del bootcamp de Data analyst de Tripleten
+Dentro de el, se ha creado una app para visualizar informacion de vehiculos en histogramas y graficos de dispersion.
+El proyecto se creo en GitHub para posteriormente trabajarse de forma local en VS Code
---
+++ app.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+car_data = pd.read_csv('Agersonlopezb/ttAaronsproject/vehicles_us.csv') # leer los datos
+st.header('Vehiculos Aaron Lopez')
+st.write('Esta es mi primer aplicacion funcional.')
+
+hist_button = st.button('Construir histograma ejemplo') # crear un botón
+     
+if hist_button: # al hacer clic en el botón
+    # escribir un mensaje
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+         
+    # crear un histograma
+    fig = px.histogram(car_data, x="odometer")
+     
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
+st.write('Grafico de dispersion.')
+fig = px.scatter(car_data, x="odometer", y="price") # crear un gráfico de dispersión
+fig.show() # crear gráfico de dispersión
+
+st.write('Con casilla de verificacion.')
+build_histogram = st.checkbox('Construir un histograma')
+
+if build_histogram: # si la casilla de verificación está seleccionada
+    st.write('Construir un histograma para la columna odómetro')
+    # crear un histograma
+    fig = px.histogram(car_data, x="odometer")
+     
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
